622a428189d7c80c03f9e4a4e5ff2dee2d611e66
andrePankraz@googlemail.com <andrePankraz@googlemail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2011-07-03 21:53:11 +0000

    GAE tests mit Blobs

---
+++ trunk/DecoJerWeb/src/org/decojer/web/UploadServlet.java
@@ -0,0 +1,69 @@
+/*
+ * $Id$
+ *
+ * This file is part of the DecoJer project.
+ * Copyright (C) 2010-2011  André Pankraz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every Java Source Code
+ * that is created using DecoJer.
+ */
+package org.decojer.web;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+public class UploadServlet extends HttpServlet {
+
+	private static Logger LOGGER = Logger.getLogger(UploadServlet.class
+			.getName());
+
+	private final BlobstoreService blobstoreService = BlobstoreServiceFactory
+			.getBlobstoreService();
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
+
+		BlobKey blobKey = null;
+		for (Entry<String, BlobKey> entry : blobs.entrySet()) {
+			LOGGER.warning("TEST: " + entry.getKey());
+			LOGGER.warning("TEST: " + entry.getValue().getKeyString());
+			if (blobKey == null)
+				blobKey = entry.getValue();
+		}
+
+		if (blobKey == null) {
+			res.sendRedirect("/");
+		} else {
+			res.sendRedirect("/decompile?blob-key=" + blobKey.getKeyString());
+		}
+	}
+
+}
