281d32c1fc7041bc1b99491b8d8cde97ae46796e
andrePankraz <andrePankraz@295ee101-0993-024e-5712-d33352bf8b4c>
2011-07-09 09:32:28 +0000

    now with object  web asm

---
+++ DecoJerWeb/src/org/decojer/web/util/EntityUtils.java
@@ -0,0 +1,63 @@
+/*
+ * $Id$
+ *
+ * This file is part of the DecoJer project.
+ * Copyright (C) 2010-2011  André Pankraz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every Java Source Code
+ * that is created using DecoJer.
+ */
+package org.decojer.web.util;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Transaction;
+
+/**
+ * 
+ * @author André Pankraz
+ */
+public class EntityUtils {
+
+	public static Entity getBlobInfoEntity(
+			final DatastoreService datastoreService, final BlobKey blobKey)
+			throws EntityNotFoundException {
+
+		final Entity blobInfoEntity = datastoreService.get(KeyFactory
+				.createKey("__BlobInfo__", blobKey.getKeyString()));
+
+		// in development mode no property "md5_hash" is created (for
+		// recognition of duplicate entities), luckily we can change this
+		// entities here (not possible on production)
+		String md5Hash = (String) blobInfoEntity.getProperty("md5_hash");
+		if (md5Hash == null) {
+			md5Hash = "_" + blobInfoEntity.getProperty("filename") + "_"
+					+ blobInfoEntity.getProperty("size");
+			blobInfoEntity.setProperty("md5_hash", md5Hash);
+			// flush for following query
+			final Transaction tx = datastoreService.beginTransaction();
+			// not allowed on production!!!
+			datastoreService.put(blobInfoEntity);
+			tx.commit();
+		}
+		return blobInfoEntity;
+	}
+
+}
---
+++ DecoJerWeb/src/org/decojer/web/util/IOUtils.java
@@ -0,0 +1,68 @@
+/*
+ * $Id$
+ *
+ * This file is part of the DecoJer project.
+ * Copyright (C) 2010-2011  André Pankraz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every Java Source Code
+ * that is created using DecoJer.
+ */
+package org.decojer.web.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 
+ * @author André Pankraz
+ */
+public class IOUtils {
+
+	public static int copy(final InputStream is, final OutputStream os)
+			throws IOException {
+		final byte[] buffer = new byte[4096];
+		long count = 0;
+		int n = 0;
+		while (-1 != (n = is.read(buffer))) {
+			os.write(buffer, 0, n);
+			count += n;
+		}
+		return count > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) count;
+	}
+
+	public static byte[] toBytes(final InputStream is) throws IOException {
+		final ByteArrayOutputStream os = new ByteArrayOutputStream(32486);
+		copy(is, os);
+		return os.toByteArray();
+	}
+
+	public static String toHexString(final byte[] bytes) {
+		final char[] hexArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+				'9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		final char[] hexChars = new char[bytes.length * 2];
+		int v;
+		for (int j = 0; j < bytes.length; j++) {
+			v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v / 16];
+			hexChars[j * 2 + 1] = hexArray[v % 16];
+		}
+		return new String(hexChars);
+	}
+
+}
