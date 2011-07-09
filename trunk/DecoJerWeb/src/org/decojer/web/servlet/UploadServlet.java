7d37bbea7937fa23d92d6c525936b0e81cc5d11a
andrePankraz <andrePankraz@295ee101-0993-024e-5712-d33352bf8b4c>
2011-07-09 09:32:28 +0000

    now with object  web asm

--- trunk/DecoJerWeb/src/org/decojer/web/servlet/UploadServlet.java
+++ trunk/DecoJerWeb/src/org/decojer/web/servlet/UploadServlet.java
@@ -25,12 +25,21 @@ package org.decojer.web.servlet;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -42,17 +51,21 @@ import org.decojer.PackageClassStreamProvider;
 import org.decojer.cavaj.model.CU;
 import org.decojer.cavaj.model.PF;
 import org.decojer.cavaj.model.TD;
+import org.decojer.web.stream.StatClassVisitor;
+import org.decojer.web.util.EntityUtils;
+import org.decojer.web.util.IOUtils;
 import org.decojer.web.util.Messages;
+import org.objectweb.asm.ClassReader;
 
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreInputStream;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.datastore.Blob;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Transaction;
 
@@ -66,6 +79,19 @@ public class UploadServlet extends HttpServlet {
 			.getName());
 
 	private static final long serialVersionUID = -6567596163814017159L;
+
+	private static int MAGIC_NUMBER_MAX_LENGTH = 4;
+
+	private static byte[] MAGIC_NUMBER = new byte[MAGIC_NUMBER_MAX_LENGTH];
+
+	private static final byte[] MAGIC_NUMBER_DEX_CLASS = { (byte) 0x64,
+			(byte) 0x65, (byte) 0x78, (byte) 0x0A };
+
+	private static final byte[] MAGIC_NUMBER_JAVA_CLASS = { (byte) 0xCA,
+			(byte) 0xFE, (byte) 0xBA, (byte) 0xBE };
+
+	private static final byte[] MAGIC_NUMBER_ZIP = { (byte) 0x50, (byte) 0x4B,
+			(byte) 0x03, (byte) 0x04 };
 
 	private final BlobstoreService blobstoreService = BlobstoreServiceFactory
 			.getBlobstoreService();
@@ -87,22 +113,16 @@ public class UploadServlet extends HttpServlet {
 			return;
 		}
 
-		// transaction only for transaction level SERIALIZABLE, always commit
-		final Transaction tx = this.datastoreService.beginTransaction();
-		try {
-			// get blob info entity
-			final Entity blobInfoEntity = this.datastoreService.get(KeyFactory
-					.createKey("__BlobInfo__", blobKey.getKeyString()));
+		final List<BlobKey> toDelete = new ArrayList<BlobKey>();
 
-			String md5Hash = (String) blobInfoEntity.getProperty("md5_hash");
-			if (md5Hash == null) {
-				// only for development mode, currently necessary
-				md5Hash = "_" + blobInfoEntity.getProperty("filename") + "_"
-						+ blobInfoEntity.getProperty("size");
-				blobInfoEntity.setProperty("md5_hash", md5Hash);
-				// not allowed on production!!!
-				this.datastoreService.put(blobInfoEntity);
-			}
+		// transaction only for transaction level SERIALIZABLE, always commit
+		Transaction tx = this.datastoreService.beginTransaction();
+		try {
+			final Entity blobInfoEntity = EntityUtils.getBlobInfoEntity(
+					this.datastoreService, blobKey);
+
+			final String md5Hash = (String) blobInfoEntity
+					.getProperty("md5_hash");
 
 			final Query duplicateQuery = new Query("__BlobInfo__");
 			duplicateQuery.addFilter("md5_hash", Query.FilterOperator.EQUAL,
@@ -110,6 +130,8 @@ public class UploadServlet extends HttpServlet {
 			final List<Entity> duplicateEntities = this.datastoreService
 					.prepare(duplicateQuery).asList(
 							FetchOptions.Builder.withLimit(10));
+			LOGGER.warning("Duplicate entities for hash '" + md5Hash + "': "
+					+ duplicateEntities.size());
 			if (duplicateEntities.size() > 1) {
 				// find oldest...
 				Entity oldestEntity = null;
@@ -147,17 +169,40 @@ public class UploadServlet extends HttpServlet {
 						oldestEntity = duplicateEntity;
 					}
 					// change and delete newer entry
-					this.blobstoreService.delete(new BlobKey(dieEntity.getKey()
-							.getName()));
-					Messages.addMessage(req, "Duplicate! Delete.");
+					toDelete.add(new BlobKey(dieEntity.getKey().getName()));
 				}
-				oldestEntity.setProperty("updated", newestDate);
-				oldestEntity.setProperty("uploaded", uploadedSum);
-				// this.datastoreService.put(oldestEntity);
 				blobKey = new BlobKey(oldestEntity.getKey().getName());
 			}
 		} catch (final Exception e) {
 			LOGGER.log(Level.WARNING, "Problems with database operations.", e);
+			Messages.addMessage(req, "Internal system problem!");
+			res.sendRedirect("/");
+			return;
+		} finally {
+			if (tx.isActive()) {
+				tx.commit();
+			}
+		}
+
+		// after transaction, entity group!
+		if (!toDelete.isEmpty()) {
+			this.blobstoreService.delete(toDelete.toArray(new BlobKey[toDelete
+					.size()]));
+			Messages.addMessage(req, "Duplicate! Delete " + toDelete.size()
+					+ " entries!");
+		}
+		// before transaction, entity group!
+		final BlobstoreInputStream blobstoreInputStream = new BlobstoreInputStream(
+				blobKey);
+
+		// transaction only for transaction level SERIALIZABLE, always commit
+		tx = this.datastoreService.beginTransaction();
+		try {
+			// TODO
+			final MessageDigest subDigest = MessageDigest.getInstance("MD5");
+			visitStream(blobstoreInputStream, "TEST", subDigest);
+		} catch (final Exception e) {
+			LOGGER.log(Level.WARNING, "Problems with stream visitor.", e);
 			Messages.addMessage(req, "Internal system problem!");
 			res.sendRedirect("/");
 			return;
@@ -188,6 +233,64 @@ public class UploadServlet extends HttpServlet {
 			return;
 		}
 		res.sendRedirect("/");
+	}
+
+	private void visitStream(final InputStream is, final String name,
+			final MessageDigest digest) throws IOException,
+			NoSuchAlgorithmException {
+		final int read = is.read(MAGIC_NUMBER, 0, MAGIC_NUMBER_MAX_LENGTH);
+		if (read < MAGIC_NUMBER_MAX_LENGTH) {
+			// System.out.println("  MN too small: " + read);
+			return;
+		}
+		if (Arrays.equals(MAGIC_NUMBER_JAVA_CLASS, MAGIC_NUMBER)) {
+			final PushbackInputStream pis = new PushbackInputStream(is, 4);
+			pis.unread(MAGIC_NUMBER, 0, MAGIC_NUMBER_MAX_LENGTH);
+
+			// rough test and statistics
+			final ClassReader classReader = new ClassReader(
+					digest == null ? pis : new DigestInputStream(pis, digest));
+			final StatClassVisitor statClassVisitor = new StatClassVisitor();
+			classReader.accept(statClassVisitor, ClassReader.SKIP_FRAMES);
+
+			final String md5Hash = IOUtils.toHexString(digest.digest());
+			LOGGER.warning("  Javaclass: " + name + md5Hash);
+			final Entity entity = new Entity("Class");
+			entity.setProperty("name", statClassVisitor.className);
+			entity.setProperty("md5_hash", md5Hash);
+			entity.setProperty("content", new Blob(classReader.b));
+			this.datastoreService.put(entity);
+			return;
+		}
+		if (Arrays.equals(MAGIC_NUMBER_DEX_CLASS, MAGIC_NUMBER)) {
+			LOGGER.warning("  Dex: " + name);
+
+			// use http://code.google.com/p/dex2jar/
+
+			return;
+		}
+		if (Arrays.equals(MAGIC_NUMBER_ZIP, MAGIC_NUMBER)) {
+			final PushbackInputStream pis = new PushbackInputStream(is, 4);
+			pis.unread(MAGIC_NUMBER, 0, MAGIC_NUMBER_MAX_LENGTH);
+
+			final MessageDigest subDigest = MessageDigest.getInstance("MD5");
+
+			final ZipInputStream zip = new ZipInputStream(digest == null ? pis
+					: new DigestInputStream(pis, digest));
+			for (ZipEntry zipEntry = zip.getNextEntry(); zipEntry != null; zipEntry = zip
+					.getNextEntry()) {
+				try {
+					visitStream(zip, zipEntry.getName(), subDigest);
+				} catch (final Exception e) {
+					LOGGER.log(Level.WARNING, "Zip entry problems: ", e);
+				}
+			}
+			LOGGER.warning("  Zip: " + name
+					+ IOUtils.toHexString(digest.digest()));
+			return;
+		}
+		// System.out.println("  Unknown!");
+		return;
 	}
 
 }
