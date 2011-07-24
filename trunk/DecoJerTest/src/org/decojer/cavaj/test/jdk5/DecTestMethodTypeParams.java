7ea45996c424b202511e4d8dbe7a36c79fdd8009
andrePankraz@googlemail.com <andrePankraz@googlemail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2011-07-24 21:52:04 +0000

    advanced type stuff

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestMethodTypeParams.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestMethodTypeParams.java
@@ -1,22 +1,25 @@
 package org.decojer.cavaj.test.jdk5;
 
+import java.util.ArrayList;
+
 import javax.accessibility.Accessible;
 
 public class DecTestMethodTypeParams<T extends Integer & Cloneable & Accessible> {
 
-	private static <T extends Integer> void parameterizedStaticClassMethod(
-			final T a, final T b) {
+	private static <T extends Integer, E extends RuntimeException> void parameterizedStaticClassMethod(
+			final T a, final T b) throws E, RuntimeException {
 		System.out.println(a);
 		System.out.println(b);
 	}
 
 	private <T extends Integer, U extends Long> void parameterizedClassMethod(
-			final T a, final U b) {
+			final T a, final U b) throws RuntimeException {
 		System.out.println(a);
 		System.out.println(b);
 	}
 
-	private <U extends Long> U parameterizedClassMethod2(final T a, final U b) {
+	private <U extends Long & Accessible> U parameterizedClassMethod2(
+			final T a, final U b) {
 		System.out.println(a);
 		System.out.println(b);
 		return (U) new Long(1L);
@@ -26,6 +29,9 @@ public class DecTestMethodTypeParams<T extends Integer & Cloneable & Accessible>
 	private <U extends T> void parameterizedClassMethod3(final T a, final U b) {
 		System.out.println(a);
 		System.out.println(b);
+		class Test extends ArrayList<U> {
+
+		}
 	}
 
 }
--- trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestMethods.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestMethods.java
@@ -1,12 +1,11 @@
 package org.decojer.cavaj.test.jdk5;
 
-
-public abstract class DecTestMethods<E extends Throwable> {
+public abstract class DecTestMethods<E extends Appendable> {
 
 	@DecTestAnnotations(byteTest = 1)
 	public final byte getByteTest(
 			@DecTestAnnotations(intTest = 1, charTest = 'c') final E test)
-			throws E, IllegalAccessError {
+			throws IllegalAccessError {
 		return 1;
 	}
 
