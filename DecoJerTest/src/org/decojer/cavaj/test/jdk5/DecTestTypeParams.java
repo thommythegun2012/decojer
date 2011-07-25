ebae8dcb9ff30e33d0da9195496f2c952a47e88f
andrePankraz <andrePankraz@295ee101-0993-024e-5712-d33352bf8b4c>
2011-07-25 12:10:40 +0000

    annotation tests restructure

--- DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestTypeParams.java
+++ DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestTypeParams.java
@@ -7,11 +7,11 @@ import java.util.Map;
 import javax.accessibility.Accessible;
 
 // multiple bounds only with "type parameters", no super, no ?
-public class DecTestTypeParams<T extends Integer & Cloneable & Accessible, U>
+public class DecTestTypeParams<T extends Integer & Cloneable & Accessible, U extends RuntimeException>
 		extends ArrayList<T> implements List<T> {
 
 	// super only with "type arguments", not multiple bounds
-	private Map<? extends T, ? super T> getBothBounds() {
+	private Map<? extends T, ? super T> getBothBounds() throws U, U {
 		return null;
 	}
 
