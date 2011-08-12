7271fc8a8d2f2769370f76a849f70eb04520eb23
andrePankraz <andrePankraz@googlemail.com>
2011-08-12 20:37:53 +0000

    exception test

--- DecoJerTest/src/org/decojer/cavaj/test/DecTestExceptions.java
+++ DecoJerTest/src/org/decojer/cavaj/test/DecTestExceptions.java
@@ -1,26 +1,7 @@
 package org.decojer.cavaj.test;
 
+
 public abstract class DecTestExceptions {
-
-	void _finally() {
-		System.out.println("PRE");
-		try {
-			System.out.println("IN");
-		} finally {
-			System.out.println("FINALLY");
-		}
-		System.out.println("POST");
-	}
-
-	void exception() {
-		System.out.println("PRE");
-		try {
-			System.out.println("IN");
-		} catch (final Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("POST");
-	}
 
 	void exceptionFinally() {
 		System.out.println("PRE");
@@ -32,6 +13,43 @@ public abstract class DecTestExceptions {
 		} catch (final Error e) {
 			System.out.println("ERR: " + e);
 			e.printStackTrace();
+		} finally {
+			System.out.println("FINALLY");
+		}
+		System.out.println("POST");
+	}
+
+	void innerException() {
+		System.out.println("PRE");
+		try {
+			try {
+				System.out.println("IN");
+			} catch (final RuntimeException e) {
+				e.printStackTrace();
+			} catch (final OutOfMemoryError e) {
+				e.printStackTrace();
+			}
+		} catch (final Error e) {
+			System.out.println("ERR: " + e);
+			e.printStackTrace();
+		}
+		System.out.println("POST");
+	}
+
+	void simpleException() {
+		System.out.println("PRE");
+		try {
+			System.out.println("IN");
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("POST");
+	}
+
+	void simpleFinally() {
+		System.out.println("PRE");
+		try {
+			System.out.println("IN");
 		} finally {
 			System.out.println("FINALLY");
 		}
