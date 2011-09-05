a6f3914d1e165c15fa627563ac847def799f445a
andrePankraz <andrePankraz@googlemail.com>
2011-09-05 20:13:41 +0000

    array tests

--- DecoJerTest/src/org/decojer/cavaj/test/DecTestFillArrays.java
+++ DecoJerTest/src/org/decojer/cavaj/test/DecTestFillArrays.java
@@ -2,7 +2,7 @@ package org.decojer.cavaj.test;
 
 public abstract class DecTestFillArrays {
 
-	public static void test() {
+	public static void testSimple() {
 		boolean[] boolsFull0 = new boolean[4];
 		boolsFull0[0] = true;
 		boolsFull0[1] = false;
@@ -20,18 +20,25 @@ public abstract class DecTestFillArrays {
 		boolean[] boolsFull3 = new boolean[] { true, false, true, false };
 
 		boolean[] bools = { true, false, true, false };
+		boolean[] boolsEmpty = {};
+
 		char[] chars = { 'a', 'b', 'c' };
 		// 0x80 is int!
 		byte[] bytes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x7f };
 		// 0x8000 is short!
-		short[] shorts = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 0xff, 0x7fff };
+		// 'a' not in jdk < 1.3
+		short[] shorts = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xff, 0x7fff };
 		int[] ints = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 0x80000000,
 				0xffffffff };
 		long[] longs = { 0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9, 'a' };
 		float[] floats = { 0F, 1F, 2F, 3 };
 		double[] doubles = { 0D, 1D, 2D, 3, 4F };
 		String[] strings = { "test0", null, "test1", null, "test2" };
-		Double[] doubless = { null, 0D, 1D, 2D };
+	}
+
+	public void testMulti() {
+		int[][] intssEmpty = { {} };
+		int[][] intss = { { 0, 1, 2, 3, 4 }, { 5, 6, 7, 8, 9, 0 }, { -1, 1 } };
 	}
 
 }
