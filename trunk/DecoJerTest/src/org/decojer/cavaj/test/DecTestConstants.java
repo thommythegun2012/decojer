6901e739a57ff321b0d341e9b700386102e68234
andrePankraz@googlemail.com <andrePankraz@googlemail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2011-09-07 19:35:20 +0000

    type constantsd tests

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestConstants.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestConstants.java
@@ -2,6 +2,20 @@ package org.decojer.cavaj.test;
 
 public class DecTestConstants {
 
+	public static void byteConstants() {
+		byte maxValue = Byte.MAX_VALUE;
+		byte minValue = Byte.MIN_VALUE;
+
+		System.out.println("TEST: " + maxValue + " : " + minValue);
+	}
+
+	public static void charConstants() {
+		char maxValue = Character.MAX_VALUE;
+		char minValue = Character.MIN_VALUE;
+
+		System.out.println("TEST: " + maxValue + " : " + minValue);
+	}
+
 	public static void doubleConstants() {
 		double maxValue = Double.MAX_VALUE;
 		double minValue = Double.MIN_VALUE;
@@ -13,7 +27,7 @@ public class DecTestConstants {
 				+ " : " + negativeInfinity + " : " + positiveInfinity);
 	}
 
-	public static void floadConstants() {
+	public static void floatConstants() {
 		float maxValue = Float.MAX_VALUE;
 		float minValue = Float.MIN_VALUE;
 		float nan = Float.NaN;
@@ -22,6 +36,20 @@ public class DecTestConstants {
 
 		System.out.println("TEST: " + maxValue + " : " + minValue + " : " + nan
 				+ " : " + negativeInfinity + " : " + positiveInfinity);
+	}
+
+	public static void longConstants() {
+		long maxValue = Long.MAX_VALUE;
+		long minValue = Long.MIN_VALUE;
+
+		System.out.println("TEST: " + maxValue + " : " + minValue);
+	}
+
+	public static void shortConstants() {
+		short maxValue = Short.MAX_VALUE;
+		short minValue = Short.MIN_VALUE;
+
+		System.out.println("TEST: " + maxValue + " : " + minValue);
 	}
 
 }
