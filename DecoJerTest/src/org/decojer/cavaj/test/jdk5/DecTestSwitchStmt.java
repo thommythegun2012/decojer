43ffb4ed93f1ba2f1eb79f3d1430a2e26ff991bd
andrePankraz <andrePankraz@googlemail.com>
2011-09-23 19:40:38 +0000

    switch enum reverse order

--- DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestSwitchStmt.java
+++ DecoJerTest/src/org/decojer/cavaj/test/jdk5/DecTestSwitchStmt.java
@@ -31,4 +31,33 @@ public abstract class DecTestSwitchStmt {
 		}
 	}
 
+	public static void testSwitchEnumRev(final DecTestEnums e) {
+		switch (e) {
+		// no ENUM1
+		case ENUM4:
+			System.out.println("ENUM4");
+			break;
+		case ENUM3:
+		case ENUM2:
+			System.out.println("ENUM2/3");
+		}
+	}
+
+	public static void testSwitchEnumRevDefault(final DecTestEnums e) {
+		switch (e) {
+		// no ENUM1
+		case ENUM4:
+			System.out.println("ENUM4");
+			break;
+		default:
+			System.out.println("default");
+			break;
+		case ENUM3:
+			System.out.println("ENUM3");
+			break;
+		case ENUM2:
+			System.out.println("ENUM2");
+		}
+	}
+
 }
