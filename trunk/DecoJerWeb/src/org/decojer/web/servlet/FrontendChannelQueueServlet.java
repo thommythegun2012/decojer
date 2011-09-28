c1eb02f0519401b5545df1b69f389e8b1267d441
andrePankraz <andrePankraz@295ee101-0993-024e-5712-d33352bf8b4c>
2011-09-28 14:53:04 +0000

    workaround for frontend channel

---
+++ trunk/DecoJerWeb/src/org/decojer/web/servlet/FrontendChannelQueueServlet.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ *
+ * This file is part of the DecoJer project.
+ * Copyright (C) 2010-2011  André Pankraz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every Java Source Code
+ * that is created using DecoJer.
+ */
+package org.decojer.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.channel.ChannelMessage;
+import com.google.appengine.api.channel.ChannelServiceFactory;
+
+/**
+ * Frontend channel queue servlet.
+ * 
+ * @author André Pankraz
+ */
+public class FrontendChannelQueueServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(final HttpServletRequest req,
+			final HttpServletResponse resp) throws ServletException,
+			IOException {
+		final String channelKey = req.getParameter("channelKey");
+		if (channelKey != null) {
+			System.out.println("#Channel_Send: " + channelKey);
+			ChannelServiceFactory.getChannelService().sendMessage(
+					new ChannelMessage(channelKey, "Decompiled!"));
+		}
+	}
+
+}
