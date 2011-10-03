9820aa45425c370bd56a36407c720c2aab3dc2fb
andrePankraz@googlemail.com <andrePankraz@googlemail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2011-10-03 20:16:26 +0000

    data flow analysis 10/100 ;)

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestCmpOperators.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestCmpOperators.java
@@ -20,4 +20,24 @@ public abstract class DecTestCmpOperators {
 		System.out.println(a > b);
 	}
 
+	public static void test0() {
+		int a = 0;
+		System.out.println(a < 0);
+		System.out.println(a <= 0);
+		System.out.println(a == 0);
+		System.out.println(a != 0);
+		System.out.println(a >= 0);
+		System.out.println(a > 0);
+	}
+
+	public static void test1() {
+		int a = 1;
+		System.out.println(a < 1);
+		System.out.println(a <= 1);
+		System.out.println(a == 1);
+		System.out.println(a != 1);
+		System.out.println(a >= 1);
+		System.out.println(a > 1);
+	}
+
 }
