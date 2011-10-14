9fed1be18d314c1c36aa65760b984d4500a82e24
andrePankraz <andrePankraz@295ee101-0993-024e-5712-d33352bf8b4c>
2011-10-14 13:26:48 +0000

    refactoring

---
+++ DecoJer/src/org/decojer/cavaj/model/code/op/CAST.java
@@ -0,0 +1,58 @@
+/*
+ * $Id$
+ *
+ * This file is part of the DecoJer project.
+ * Copyright (C) 2010-2011  André Pankraz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every Java Source Code
+ * that is created using DecoJer.
+ */
+package org.decojer.cavaj.model.code.op;
+
+import org.decojer.cavaj.model.T;
+
+/**
+ * Operation 'CAST'.
+ * 
+ * @author André Pankraz
+ */
+public class CAST extends Operation {
+
+	private final T t;
+
+	private final T toT;
+
+	public CAST(final int pc, final int code, final int line, final T t, final T toT) {
+		super(pc, code, line);
+		this.t = t;
+		this.toT = toT;
+	}
+
+	@Override
+	public Opcode getOpcode() {
+		return Opcode.CAST;
+	}
+
+	public T getT() {
+		return this.t;
+	}
+
+	public T getToT() {
+		return this.toT;
+	}
+
+}
