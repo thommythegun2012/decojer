c600f34a7c541dd3b55f2a8631ec02799cdd7216
andrePankraz@googlemail.com <andrePankraz@googlemail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2011-10-16 12:30:57 +0000

    set proper targets

--- trunk/DecoJerTest/script.bat
+++ trunk/DecoJerTest/script.bat
@@ -24,25 +24,25 @@ del /S/Q D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.7.0\*.class
 "C:\Program Files\Java\jdk1.1.8\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.1.8
 
 
-"C:\Program Files\Java\jdk1.2.1\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.1
-"C:\Program Files\Java\jdk1.2.1\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.1
+"C:\Program Files\Java\jdk1.2.1\bin\javac" -target 1.2 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.1
+"C:\Program Files\Java\jdk1.2.1\bin\javac" -target 1.2 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.1
 
-"C:\Program Files\Java\jdk1.2.2\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.2
-"C:\Program Files\Java\jdk1.2.2\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.2
-
-
-"C:\Program Files\Java\jdk1.3\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3
-"C:\Program Files\Java\jdk1.3\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3
-
-"C:\Program Files\Java\jdk1.3.1_20\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3.1_20
-"C:\Program Files\Java\jdk1.3.1_20\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3.1_20
+"C:\Program Files\Java\jdk1.2.2\bin\javac" -target 1.2 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.2
+"C:\Program Files\Java\jdk1.2.2\bin\javac" -target 1.2 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.2.2
 
 
-"C:\Program Files\Java\j2sdk1.4.0\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.0
-"C:\Program Files\Java\j2sdk1.4.0\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.0
+"C:\Program Files\Java\jdk1.3\bin\javac" -target 1.3 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3
+"C:\Program Files\Java\jdk1.3\bin\javac" -target 1.3 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3
 
-"C:\Program Files\Java\j2sdk1.4.2_19\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.2_19
-"C:\Program Files\Java\j2sdk1.4.2_19\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.2_19
+"C:\Program Files\Java\jdk1.3.1_20\bin\javac" -target 1.3 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3.1_20
+"C:\Program Files\Java\jdk1.3.1_20\bin\javac" -target 1.3 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.3.1_20
+
+
+"C:\Program Files\Java\j2sdk1.4.0\bin\javac" -target 1.4 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.0
+"C:\Program Files\Java\j2sdk1.4.0\bin\javac" -target 1.4 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.0
+
+"C:\Program Files\Java\j2sdk1.4.2_19\bin\javac" -target 1.4 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.2_19
+"C:\Program Files\Java\j2sdk1.4.2_19\bin\javac" -target 1.4 D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\jdk2\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_j2sdk1.4.2_19
 
 
 "C:\Program Files\Java\jdk1.5.0\bin\javac" D:\Data\Decomp\workspace\DecoJerTest\src\org\decojer\cavaj\test\*.java -g -d D:\Data\Decomp\workspace\DecoJerTest\bin_jdk1.5.0
