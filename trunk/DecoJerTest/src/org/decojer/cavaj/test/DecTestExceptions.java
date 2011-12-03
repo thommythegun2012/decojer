37486305b4100e99d5e9e918a7b5e57472ac43ae
andrePankraz@googlemail.com <andrePankraz@googlemail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2011-12-03 22:44:16 +0000

    no exception change-recognition for direct GOTOs (see simpleFinally for Eclipse)

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestExceptions.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestExceptions.java
@@ -2,16 +2,26 @@ package org.decojer.cavaj.test;
 
 public abstract class DecTestExceptions {
 
+	void exception() {
+		System.out.println("PRE");
+		try {
+			System.out.println("TRY");
+		} catch (final Exception e) {
+			System.out.println("EXC" + e);
+		} catch (final Error e) {
+			System.out.println("ERR" + e);
+		}
+		System.out.println("POST");
+	}
+
 	void exceptionFinally() {
 		System.out.println("PRE");
 		try {
-			System.out.println("IN");
+			System.out.println("TRY");
 		} catch (final Exception e) {
-			System.out.println("EXC: " + e);
-			e.printStackTrace();
+			System.out.println("EXC" + e);
 		} catch (final Error e) {
-			System.out.println("ERR: " + e);
-			e.printStackTrace();
+			System.out.println("ERR" + e);
 		} finally {
 			System.out.println("FINALLY");
 		}
@@ -22,20 +32,90 @@ public abstract class DecTestExceptions {
 		System.out.println("PRE");
 		try {
 			try {
-				System.out.println("IN");
-			} catch (final RuntimeException e) {
-				e.printStackTrace();
-			} catch (final OutOfMemoryError e) {
-				e.printStackTrace();
+				System.out.println("TRY");
+			} catch (final Exception e) {
+				System.out.println("IN_EXC" + e);
+			} catch (final Error e) {
+				System.out.println("IN_ERR" + e);
 			}
-		} catch (final RuntimeException e) {
-			System.out.println("ERR: " + e);
-			e.printStackTrace();
+		} catch (final Exception e) {
+			System.out.println("EXC" + e);
+		} catch (final Error e) {
+			System.out.println("ERR" + e);
 		}
 		System.out.println("POST");
 	}
 
-	boolean returnInException(int a) {
+	void innerFinally() {
+		System.out.println("PRE");
+		try {
+			try {
+				System.out.println("TRY");
+			} catch (final Exception e) {
+				System.out.println("IN_EXC" + e);
+			} catch (final Error e) {
+				System.out.println("IN_ERR" + e);
+			} finally {
+				System.out.println("IN_FIN");
+			}
+		} catch (final Exception e) {
+			System.out.println("EXC" + e);
+		} catch (final Error e) {
+			System.out.println("ERR" + e);
+		} finally {
+			System.out.println("FIN");
+		}
+		System.out.println("POST");
+	}
+
+	void innerSimpleFinally() {
+		System.out.println("PRE");
+		try {
+			try {
+				System.out.println("TRY");
+			} finally {
+				System.out.println("IN_FINALLY");
+			}
+		} finally {
+			System.out.println("FIN");
+		}
+		System.out.println("POST");
+	}
+
+	boolean returnInInnerFinally(int a) {
+		System.out.println("PRE");
+		out: try {
+			try {
+				if (a > -1) {
+					return false;
+				}
+				if (a > 1) {
+					System.out.println("INIF");
+					return true;
+				}
+				System.out.println("TRY");
+				return a > 10;
+			} catch (final Exception e) {
+				System.out.println("IN_EXC" + e);
+			} catch (final Error e) {
+				System.out.println("IN_ERR" + e);
+			} finally {
+				System.out.println("IN_FIN");
+				if (a > 0)
+					break out;
+			}
+		} catch (final Exception e) {
+			System.out.println("EXC" + e);
+		} catch (final Error e) {
+			System.out.println("ERR" + e);
+		} finally {
+			System.out.println("FIN");
+		}
+		System.out.println("POST");
+		return false;
+	}
+
+	boolean returnInSimpleException(int a) {
 		System.out.println("PRE");
 		try {
 			if (a > -1) {
@@ -45,10 +125,10 @@ public abstract class DecTestExceptions {
 				System.out.println("INIF");
 				return true;
 			}
-			System.out.println("IN");
+			System.out.println("TRY");
 			return a > 10;
 		} catch (final Exception e) {
-			e.printStackTrace();
+			System.out.println("EXC" + e);
 		}
 		System.out.println("POST");
 		return false;
@@ -57,9 +137,9 @@ public abstract class DecTestExceptions {
 	void simpleException() {
 		System.out.println("PRE");
 		try {
-			System.out.println("IN");
+			System.out.println("TRY");
 		} catch (final Exception e) {
-			e.printStackTrace();
+			System.out.println("EXC" + e);
 		}
 		System.out.println("POST");
 	}
@@ -67,9 +147,9 @@ public abstract class DecTestExceptions {
 	void simpleFinally() {
 		System.out.println("PRE");
 		try {
-			System.out.println("IN");
+			System.out.println("TRY");
 		} finally {
-			System.out.println("FINALLY");
+			System.out.println("FIN");
 		}
 		System.out.println("POST");
 	}
