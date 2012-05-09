51b20d996efee54128d614feed936669603cd595
andrePankraz <andrePankraz@295ee101-0993-024e-5712-d33352bf8b4c>
2012-05-09 07:28:12 +0000

    field tests for final

--- DecoJerTest/src/org/decojer/cavaj/test/DecTestFields.java
+++ DecoJerTest/src/org/decojer/cavaj/test/DecTestFields.java
@@ -4,34 +4,6 @@ import java.io.Serializable;
 
 public class DecTestFields {
 
-	public final byte byteTest = 127;
-
-	public char charTest = 'c';
-
-	public double doubleTest = .5432154321D; // not final!
-
-	public boolean dynamicBooleanTest = doubleTest > 1D;
-
-	public final static float floatTest = .54321F;
-
-	protected final int intTest = 1000000;
-
-	protected final static long longTest = 1000000000L;
-
-	protected final Class referenceClassTest = String.class;
-
-	protected final Class referenceClassTest2 = int.class;
-
-	protected final Class referenceClassTest3 = int[][].class.getClass();
-
-	private final String referenceStringTest = "test";
-
-	private final short shortTest = 32767;
-
-	private static final boolean booleanTest = true;
-
-	private static final int[] referenceIntArrayTest = { 1000000 };
-
 	public static void main(String[] args) {
 		System.out.println("TEST : " + String.class);
 		System.out.println("TEST : " + String[].class);
@@ -55,7 +27,110 @@ public class DecTestFields {
 		System.out.println("TEST : " + int.class.getClass().getSuperclass());
 	}
 
-	private final int[][] referenceIntArrayNullTest = null;
+	boolean booleanTest = true;
+
+	final boolean booleanTestFinal = true;
+
+	static boolean booleanTestStatic = true;
+
+	static final boolean booleanTestStaticFinal = true;
+
+	byte byteTest = 127;
+
+	final byte byteTestFinal = 127;
+
+	static byte byteTestStatic = 127;
+
+	static final byte byteTestStaticFinal = 127;
+
+	char charTest = 'c';
+
+	final char charTestFinal = 'c';
+
+	static char charTestStatic = 'c';
+
+	static final char charTestStaticFinal = 'c';
+
+	double doubleTest = .5432154321D;
+
+	final double doubleTestFinal = .5432154321D;
+
+	static public double doubleTestStatic = .5432154321D;
+
+	static public final double doubleTestStaticFinal = .5432154321D;
+
+	// following 4 only precalculated for final in expression!
+	private boolean dynamicBooleanTest = doubleTestFinal > 1D;
+
+	private final boolean dynamicBooleanTestFinal = doubleTestFinal > 1D;
+
+	private static boolean dynamicBooleanTestStatic = doubleTestStaticFinal > 1D;
+
+	private static final boolean dynamicBooleanTestStaticFinal = doubleTestStaticFinal > 1D;
+
+	public float floatTest = .54321F;
+
+	public final float floatTestFinal = .54321F;
+
+	public static float floatTestStatic = .54321F;
+
+	public static final float floatTestStaticFinal = .54321F;
+
+	public int intTest = 1000000;
+
+	public final int intTestFinal = 1000000;
+
+	public static int intTestStatic = 1000000;
+
+	public static final int intTestStaticFinal = 1000000;
+
+	public long longTest = 1000000000L;
+
+	public final long longTestFinal = 1000000000L;
+
+	public static long longTestStatic = 1000000000L;
+
+	public static final long longTestStaticFinal = 1000000000L;
+
+	protected Class referenceClassTest = String.class;
+
+	protected final Class referenceClassTestFinal = int.class;
+
+	protected static Class referenceClassTestStatic = int[][].class.getClass();
+
+	protected static final Class referenceClassTestStaticFinal = void.class;
+
+	protected int[] referenceIntArrayTest = { 1000000 };
+
+	protected final int[] referenceIntArrayTestFinal = { 1000000 };
+
+	protected static int[] referenceIntArrayTestStatic = { 1000000 };
+
+	protected static final int[] referenceIntArrayTestStaticFinal = { 1000000 };
+
+	protected int[][] referenceIntArrayNullTest = null;
+
+	protected final int[][] referenceIntArrayNullTestFinal = null;
+
+	protected static int[][] referenceIntArrayNullTestStatic = null;
+
+	protected static final int[][] referenceIntArrayNullTestStaticFinal = null;
+
+	protected String referenceStringTest = "test";
+
+	protected final String referenceStringTestFinal = "test";
+
+	protected static String referenceStringTestStatic = "test";
+
+	protected static final String referenceStringTestStaticFinal = "test";
+
+	public short shortTest = 32767;
+
+	public final short shortTestFinal = 32767;
+
+	public static short shortTestStatic = 32767;
+
+	public static final short shortTestStaticFinal = 32767;
 
 	public char getCharTest() {
 		return charTest;
