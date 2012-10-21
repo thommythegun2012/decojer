eaf0469076a74fb484e1686f054b2c44d44638f6
andrePankraz@googlemail.com <andrePankraz@googlemail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2012-10-21 21:34:26 +0000

    more compound tests

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestBooleanOperators.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/DecTestBooleanOperators.java
@@ -3,32 +3,32 @@ package org.decojer.cavaj.test;
 public abstract class DecTestBooleanOperators {
 
 	public static void conditionalCompound(final boolean a, final boolean b,
-			final boolean c, final boolean d) {
+			final boolean c, final boolean d, final boolean e) {
 		// creates none-flat forward-CFGs since JDK 4
-		System.out.println(a && d ? b : c);
-		System.out.println(a && (d ? b : c));
-		System.out.println(a || d ? b : c);
-		System.out.println(a || (d ? b : c));
+		System.out.println(a && d ? b : c && e);
+		System.out.println(a && (d ? b : c) && e);
+		System.out.println(a || d ? b : c || e);
+		System.out.println(a || (d ? b : c) || e);
 
-		System.out.println(a ? b && d : c);
-		System.out.println(!a ? b || d : c);
-		System.out.println(a ? b || d : c);
-		System.out.println(!a ? b : c || d);
+		System.out.println(a ? b && d : c || e);
+		System.out.println(!a ? b || d : c && e);
+		System.out.println(a ? b || d && e : c);
+		System.out.println(!a ? b : c || d && e);
 
-		System.out.println(a && d ? b && d : c || d);
-		System.out.println(a || d ? b || d : c && d);
+		System.out.println(a && d ? b && e : c || e);
+		System.out.println(a || d ? b || e : c && e);
 
-		System.out.println(a && d ? true : c || d);
-		System.out.println(a && d ? b && d : false);
-		System.out.println(a || d ? false : c && d);
-		System.out.println(a || d ? b || d : true);
+		System.out.println(a && d ? true : c || e);
+		System.out.println(a && d ? b && e : false);
+		System.out.println(a || d ? false : c && e);
+		System.out.println(a || d ? b || e : true);
 
-		System.out.println(a && (d ? true : c || d));
-		System.out.println(a && (d ? b && d : false));
-		System.out.println(a || (d ? false : c && d));
-		System.out.println(a || (d ? b || d : true));
+		System.out.println(a && (d ? true : c || e));
+		System.out.println(a && (d ? b && e : false));
+		System.out.println(a || (d ? false : c && e));
+		System.out.println(a || (d ? b || e : true));
 
-		System.out.println(a && d ? a || d ? b && d : c || d : !b && c || d);
+		System.out.println(a && d ? a || e ? b && e : c || e : !b && c || e);
 	}
 
 	public static void conditionalCompoundValue(final boolean a,
