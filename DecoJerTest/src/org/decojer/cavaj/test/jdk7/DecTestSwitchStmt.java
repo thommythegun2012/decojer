9d42c135d1b17238c57c0adee82f329f15a9e429
andrePankraz <andrePankraz@295ee101-0993-024e-5712-d33352bf8b4c>
2012-11-12 08:00:52 +0000

    hash collision test

--- DecoJerTest/src/org/decojer/cavaj/test/jdk7/DecTestSwitchStmt.java
+++ DecoJerTest/src/org/decojer/cavaj/test/jdk7/DecTestSwitchStmt.java
@@ -4,12 +4,15 @@ public abstract class DecTestSwitchStmt {
 
 	public static void testSwitchString(final String s) {
 		switch (s) {
+		case "BB":
 		case "STR2":
 			System.out.println("STRING2");
 			break;
 		case "STR3":
 		case "STR4":
 			System.out.println("STRING3/4");
+		case "Aa":
+			System.out.println("Hash Collision with BB and Fall Through...");
 		}
 	}
 
