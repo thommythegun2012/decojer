76e06f3e89ace44f1c979c1406344f24ca9fde42
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2013-01-06 20:28:16 +0000

    fix multinewarray ...we will test the other direction in a few moments.

--- trunk/DecoJer/src/org/decojer/cavaj/readers/javassist/ReadCodeAttribute.java
+++ trunk/DecoJer/src/org/decojer/cavaj/readers/javassist/ReadCodeAttribute.java
@@ -977,8 +977,14 @@ public class ReadCodeAttribute {
 			case Opcode.MULTIANEWARRAY: {
 				final int classIndex = codeReader.readUnsignedShort();
 				final int dimensions = codeReader.readUnsignedByte();
-				this.ops.add(new NEWARRAY(this.ops.size(), opcode, line,
-						getT(constPool, classIndex), dimensions));
+				// operation works different from other newarrays, descriptor contains array with
+				// dimension > given sizes on stack, e.g.: new int[1][2][3][][], dimension is 3 and
+				// descriptor is [[[[[I, reduce!
+				t = getT(constPool, classIndex);
+				for (int i = dimensions; i-- > 0;) {
+					t = t.getComponentT();
+				}
+				this.ops.add(new NEWARRAY(this.ops.size(), opcode, line, t, dimensions));
 				break;
 			}
 			/*******
