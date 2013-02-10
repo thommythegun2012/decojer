48221ba98b36213608d991cfb1024a7b0004c139
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2013-02-10 13:10:31 +0000

    still need literal separately, cleanup

--- trunk/DecoJer/src/org/decojer/cavaj/utils/Annotations.java
+++ trunk/DecoJer/src/org/decojer/cavaj/utils/Annotations.java
@@ -23,6 +23,9 @@
  */
 package org.decojer.cavaj.utils;
 
+import static org.decojer.cavaj.utils.Expressions.decompileType;
+import static org.decojer.cavaj.utils.Expressions.decompileTypeName;
+
 import java.lang.reflect.Array;
 import java.util.List;
 import java.util.Set;
@@ -77,13 +80,13 @@ public final class Annotations {
 				if (expression != null) {
 					final SingleMemberAnnotation singleMemberAnnotation = ast
 							.newSingleMemberAnnotation();
-					singleMemberAnnotation.setTypeName(Expressions.decompileName(a.getT(), td));
+					singleMemberAnnotation.setTypeName(decompileTypeName(a.getT(), td));
 					singleMemberAnnotation.setValue(expression);
 					return singleMemberAnnotation;
 				}
 			}
 			final NormalAnnotation normalAnnotation = ast.newNormalAnnotation();
-			normalAnnotation.setTypeName(Expressions.decompileName(a.getT(), td));
+			normalAnnotation.setTypeName(decompileTypeName(a.getT(), td));
 			for (final String memberName : memberNames) {
 				final Expression expression = decompileAnnotationDefaultValue(td,
 						a.getMemberValue(memberName));
@@ -99,7 +102,7 @@ public final class Annotations {
 			}
 		}
 		final MarkerAnnotation markerAnnotation = ast.newMarkerAnnotation();
-		markerAnnotation.setTypeName(Expressions.decompileName(a.getT(), td));
+		markerAnnotation.setTypeName(decompileTypeName(a.getT(), td));
 		return markerAnnotation;
 	}
 
@@ -150,7 +153,7 @@ public final class Annotations {
 		}
 		if (defaultValue instanceof T) {
 			final TypeLiteral typeLiteral = ast.newTypeLiteral();
-			typeLiteral.setType(Expressions.decompileType((T) defaultValue, td));
+			typeLiteral.setType(decompileType((T) defaultValue, td));
 			return typeLiteral;
 		}
 		if (defaultValue instanceof Double) {
@@ -161,7 +164,7 @@ public final class Annotations {
 			if (!f.check(AF.ENUM)) {
 				LOGGER.warning("Default value field must be enum!");
 			}
-			return ast.newQualifiedName(Expressions.decompileName(f.getT(), td),
+			return ast.newQualifiedName(decompileTypeName(f.getT(), td),
 					ast.newSimpleName(f.getName()));
 		}
 		if (defaultValue instanceof Float) {
