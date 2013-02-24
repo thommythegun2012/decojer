c5a8ddc7cfe0861d29bf46c7a57e3716a6436614
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2013-02-24 19:44:53 +0000

    string switch

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk7/DecTestSwitchString.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk7/DecTestSwitchString.java
@@ -5,43 +5,44 @@ public abstract class DecTestSwitchString {
 	public static void testSwitchString(final String s) {
 		switch (s) {
 		case "BB":
-		case "STR2":
-			System.out.println("STRING2");
+		case "STR0":
+			System.out.println("BB");
 			break;
-		case "STR3":
-		case "STR4":
-			System.out.println("STRING3/4");
+		case "STR1":
+		case "STR2":
+			System.out.println("STR1/2");
 		case "Aa":
-			System.out.println("Hash Collision with BB and Fall Through...");
+			System.out
+					.println("AA ... Hash Collision with BB and Fall Through...");
 		}
 	}
 
 	public static void testSwitchStringDefault(final String s) {
 		switch (s) {
-		case "STR2":
-			System.out.println("STRING2");
+		case "STR0":
+			System.out.println("STR0");
 			break;
 		default:
 			System.out.println("default");
 			break;
-		case "STR3":
-			System.out.println("STRING3");
+		case "STR1":
+			System.out.println("STR1");
 			break;
-		case "STR4":
-			System.out.println("STRING4");
+		case "STR2":
+			System.out.println("STR2");
 		}
 	}
 
 	public static void testSwitchStringDefault2(final String s) {
 		switch (s) {
 		case "STR0":
-			System.out.println("STRING3");
+			System.out.println("STR0");
 		case "STR1":
 		case "STR2":
 		default:
 			System.out.println("default");
-		case "STR4":
-			System.out.println("STRING4");
+		case "STR3":
+			System.out.println("STR3");
 		}
 	}
 
