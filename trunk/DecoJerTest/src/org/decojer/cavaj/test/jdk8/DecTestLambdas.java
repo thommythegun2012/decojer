2f61de5175d141c4dcb660f2b7f67cbaba30b5c0
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2013-10-27 08:08:09 +0000

    start jdk8 stuff

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestLambdas.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestLambdas.java
@@ -2,4 +2,10 @@ package org.decojer.cavaj.test.jdk8;
 
 public abstract class DecTestLambdas {
 
+	public static void thread() {
+		new Thread(() -> {
+			System.out.println("Hello from a thread");
+		}).start();
+	}
+
 }
