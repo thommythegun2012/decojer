7fea00309a1408026bc331257a661eeb22a6715e
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2013-12-29 14:53:37 +0000

    improve tests

---
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk8/Nonempty.java
@@ -0,0 +1,10 @@
+package org.decojer.cavaj.test.jdk8;
+
+@java.lang.annotation.Documented
+@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Target(value = {
+		java.lang.annotation.ElementType.TYPE_USE,
+		java.lang.annotation.ElementType.TYPE_PARAMETER })
+public abstract @interface Nonempty {
+
+}
