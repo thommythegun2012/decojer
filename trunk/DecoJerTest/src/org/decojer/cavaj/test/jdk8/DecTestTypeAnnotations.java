f434881e293aa8af8c94ff810f0f0f6b85380001
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2013-12-30 14:38:22 +0000

    type annotations tests

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestTypeAnnotations.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestTypeAnnotations.java
@@ -2,14 +2,30 @@ package org.decojer.cavaj.test.jdk8;
 
 public class DecTestTypeAnnotations {
 
-	public String test() {
+	public static String test() {
 		CharSequence str = "test";
 		return (@Nonnull String) str;
 	}
 
-	public String test2() {
+	public static @Nonnull
+	String testParam(@Nonnull @Size(max = 5) CharSequence str) {
+		return (@Nonnull @Size(min = 1, max = 10) String) str;
+	}
+
+	public static <U, @Nonnull @Size(max = 1) V extends @Size(max = 2) @Nonnull String, @Nonnull W> @Nonnull @Size(max = 3) V testTypeParam(
+			final @Nonnull U muh) {
+		@Size(max = 4)
+		@Nonnull
 		CharSequence str = "test";
-		return (@Nonnull @Nonempty String) str;
+		return (@Nonnull @Size(max = 5) V) str;
+	}
+
+	public void testTryCatch() {
+		try {
+			System.out.println("TEST");
+		} catch (final @Size(max = 2) Exception e) {
+			System.out.println("CATCH");
+		}
 	}
 
 }
