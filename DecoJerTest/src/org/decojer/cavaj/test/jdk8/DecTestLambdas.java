694d642a83e6fbc5762de90c505897dc13d58fd4
andrePankraz <andrePankraz@gmail.com>
2013-12-30 17:17:16 +0000

    more lamda tests

--- DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestLambdas.java
+++ DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestLambdas.java
@@ -2,6 +2,18 @@ package org.decojer.cavaj.test.jdk8;
 
 public abstract class DecTestLambdas {
 
+	interface Test {
+		String hello(String firstname, String lastname);
+	}
+
+	public static void test() {
+		final Test test = (String firstname, String lastname) -> {
+			return "Hello " + firstname + " " + lastname;
+		};
+		System.out.println(test.hello("André", "Pankraz"));
+
+	}
+
 	public static void thread() {
 		new Thread(() -> {
 			System.out.println("Hello from a thread");
