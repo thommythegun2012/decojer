cd8b178a86151dfc43ef4306b449993a00b80c64
andrePankraz <andrePankraz@gmail.com>
2014-01-03 20:21:41 +0000

    lambda stress test ... inner classes etc

--- DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestLambdas.java
+++ DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestLambdas.java
@@ -1,9 +1,15 @@
 package org.decojer.cavaj.test.jdk8;
 
+import java.io.Serializable;
+
 public abstract class DecTestLambdas {
 
 	interface Test {
 		String hello(String firstname, String lastname);
+	}
+
+	interface TestVarargs {
+		String hello(String firstname, String... lastname);
 	}
 
 	public static void test() {
@@ -23,7 +29,32 @@ public abstract class DecTestLambdas {
 		System.out.println(test.hello("André", "Pankraz"));
 	}
 
-	public static void thread() {
+	public static void testInner() {
+		final TestVarargs test = (final @Deprecated @Nonnull String firstname,
+				String... lastname) -> {
+
+			class Test implements Serializable {
+
+				private static final long serialVersionUID = 6737746337135796923L;
+
+				private String greetings = "Hello";
+
+				public String getGreetings() {
+					return greetings;
+				}
+
+			}
+
+			Test greetings = new Test();
+
+			return greetings.getGreetings() + " " + firstname + " "
+					+ lastname[0];
+		};
+		System.out.println(test.hello("André", "Pankraz"));
+
+	}
+
+	public static void testThread() {
 		new Thread(() -> {
 			System.out.println("Hello from a thread");
 		}).start();
