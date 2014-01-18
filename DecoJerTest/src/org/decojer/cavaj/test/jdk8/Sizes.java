81a7be2a12409ee2adce1814ec4c3f04d9929378
andrePankraz <andrePankraz@gmail.com>
2014-01-18 23:10:11 +0000

    @size is now repeatable

--- DecoJerTest/src/org/decojer/cavaj/test/jdk8/Sizes.java
+++ DecoJerTest/src/org/decojer/cavaj/test/jdk8/Sizes.java
@@ -1,17 +1,12 @@
 package org.decojer.cavaj.test.jdk8;
 
-import java.lang.annotation.Repeatable;
-
 @java.lang.annotation.Documented
 @java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
 @java.lang.annotation.Target(value = {
 		java.lang.annotation.ElementType.TYPE_USE,
 		java.lang.annotation.ElementType.TYPE_PARAMETER })
-@Repeatable(Sizes.class)
-public @interface Size {
+public @interface Sizes {
 
-	int max();
-
-	int min() default 0;
+	Size[] value();
 
 }
