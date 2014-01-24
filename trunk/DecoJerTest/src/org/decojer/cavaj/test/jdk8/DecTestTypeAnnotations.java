cf265f4189d975f33595f2024ecbb1aa8bef3067
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2014-01-24 19:08:36 +0000

    use more sizes for type annotations for easy debug

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestTypeAnnotations.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk8/DecTestTypeAnnotations.java
@@ -2,35 +2,38 @@ package org.decojer.cavaj.test.jdk8;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 // @Nonnull java.util.HashMap not allowed...strange spec
-public class DecTestTypeAnnotations<U, @Nonnull V extends java.util.@Nonnull @Size(max = 1) @Size(min = 1, max = 10) HashMap<String, @Nonnull @Size(max = 11) Integer> & @Size(max = 12) @Nonnull CharSequence>
+public class DecTestTypeAnnotations<U, @Size(max = 1) @Nonnull V extends java.util.@Nonnull @Size(min = 1, max = 2) @Size(max = 3) HashMap<String, @Nonnull @Size(max = 4) Integer> & @Size(max = 5) @Nonnull CharSequence>
 		extends
-		@Nonnull HashMap<@Nonnull U, @Size(max = 13) @Nonnull List<List<@Nonnull List<@Nonnull ? extends @Nonnull String>>>>
+		@Size(max = 6) HashMap<@Size(max = 7) U, @Size(max = 8) @Nonnull List<List<@Size(max = 9) List<@Size(max = 10) ? extends @Size(max = 11) String>>>>
 		implements @Nonnull @Size(max = 14) Serializable {
 
 	class Outer<W> {
 
 		class Middle {
 
-			class Inner extends Outer<@Nonnull String>.@Nonnull Middle {
+			class Inner extends
+					Outer<@Size(max = 21) String>.@Size(max = 22) Middle {
 
 				@Nonnull
 				private final Integer arg;
 
-				public Inner(@Nonnull Integer arg) {
+				public Inner(@Size(max = 23) Integer arg) {
 					this.arg = arg;
 				}
 
 				@Nonnull
 				Inner testInnerNew(
-						@Size(max = 15) Outer<W>.@Nonnull @Size(max = 16) Middle.@Size(max = 17) Inner this,
-						@Nonnull Integer arg) {
+						@Size(max = 24) Outer<W>.@Nonnull @Size(max = 25) Middle.@Size(max = 26) Inner this,
+						@Size(max = 27) Integer arg) {
 					// Eclipse Bug: Outer<String> not allowed
-					return new Outer.@Nonnull Middle.@Size(max = 18) Inner(arg);
+					return new Outer.@Size(max = 28) Middle.@Size(max = 29) Inner(
+							arg);
 				}
 			}
 
@@ -39,43 +42,44 @@ public class DecTestTypeAnnotations<U, @Nonnull V extends java.util.@Nonnull @Si
 	}
 
 	public static @Deprecated
-	@Size(max = 19)
-	Map<@Nonnull ? super Short, ? super @Nonnull @Size(max = 20) String> testStatic;
+	@Size(max = 31)
+	Map<@Size(max = 32) ? super Short, ? super @Nonnull @Size(max = 33) String> testStatic;
 
 	public static String testArrays() {
-		CharSequence[] @Size(max = 21) [] @Nonnull @Size(max = 22) [][] test = new @Nonnull CharSequence @Nonnull [10][][] @Nonnull [];
-		return (@Nonnull String) test[0][1][2][3];
+		List<@Size(max = 41) CharSequence>[] @Size(max = 42) [] @Nonnull @Size(max = 43) [][] test = new @Size(max = 44) ArrayList @Size(max = 45) [10][][] @Size(max = 47) @Size(max = 48) [];
+		return (@Size(max = 49) String) test[0][1][2][3].get(0);
 	}
 
-	public static @Nonnull
-	String testParam(@Nonnull @Size(max = 23) CharSequence str) {
-		return (@Nonnull @Size(max = 24) String) str;
+	public static @Size(max = 51)
+	String testParam(@Nonnull @Size(max = 52) CharSequence str) {
+		return (@Nonnull @Size(max = 53) String) str;
 	}
 
-	public static <U, @Nonnull @Size(max = 25) V extends @Size(max = 26) @Nonnull String, @Nonnull W> @Nonnull @Size(max = 27) V testTypeParam(
-			final @Nonnull U muh) {
-		@Size(max = 28)
+	public static <U, @Nonnull @Size(max = 61) V extends @Size(max = 62) @Nonnull String, @Size(max = 63) W> @Nonnull @Size(max = 64) V testTypeParam(
+			final @Size(max = 65) U muh) {
+		@Size(max = 66)
 		@Nonnull
 		CharSequence str = "test";
-		return (@Nonnull @Size(max = 29) V) str;
+		return (@Nonnull @Size(max = 67) V) str;
 	}
 
 	public @Deprecated
-	@Size(max = 30)
-	Map<Short, @Nonnull @Size(max = 31) String> test;
+	@Size(max = 34)
+	Map<Short, @Nonnull @Size(max = 35) String> test;
 
-	public void testThrows() throws IOException, @Nonnull RuntimeException,
-			@Size(max = 32) @Nonnull NullPointerException {
+	public void testThrows() throws IOException,
+			@Size(max = 71) RuntimeException,
+			@Size(max = 72) @Nonnull NullPointerException {
 		System.out.println("TEST");
 	}
 
 	public void testTryCatch() {
 		try {
 			System.out.println("TEST");
-		} catch (final @Size(max = 33) NullPointerException e) {
+		} catch (final @Size(max = 81) NullPointerException e) {
 			System.out.println("CATCH");
-		} catch (final @Size(max = 34) RuntimeException
-				| @Size(max = 35) @Nonnull Error e) {
+		} catch (final @Size(max = 82) @Size(max = 83) RuntimeException
+				| @Size(max = 84) @Nonnull Error e) {
 			System.out.println("MULTI_CATCH");
 		}
 	}
