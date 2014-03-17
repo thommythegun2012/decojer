f798e90624b658f62efa63c8cecae4a5694c72ad
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2014-03-17 17:05:27 +0000

    small test with null checks

--- trunk/DecoJer/src/org/decojer/cavaj/model/types/BaseT.java
+++ trunk/DecoJer/src/org/decojer/cavaj/model/types/BaseT.java
@@ -16,7 +16,7 @@
 
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License,
  * a covered work must retain the producer line in every Java Source Code
  * that is created using DecoJer.
@@ -34,7 +34,7 @@ import org.eclipse.jdt.core.dom.ASTNode;
 
 /**
  * Base type.
- * 
+ *
  * @author André Pankraz
  */
 public abstract class BaseT extends T {
--- trunk/DecoJer/src/org/decojer/cavaj/model/types/QualifiedT.java
+++ trunk/DecoJer/src/org/decojer/cavaj/model/types/QualifiedT.java
@@ -16,12 +16,14 @@
 
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License,
  * a covered work must retain the producer line in every Java Source Code
  * that is created using DecoJer.
  */
 package org.decojer.cavaj.model.types;
+
+import javax.annotation.Nullable;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,7 +33,7 @@ import org.decojer.cavaj.model.DU;
  * Qualified type. This is used for references with parameterization and annotations of enclosing
  * classes. We cannot set it in the unmodified class type enclosing info, because it can differ
  * between references.
- * 
+ *
  * @author André Pankraz
  */
 @Slf4j
@@ -44,7 +46,7 @@ public class QualifiedT extends ExtendedT {
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param qualifierT
 	 *            type qualifier
 	 * @param t
@@ -59,6 +61,7 @@ public class QualifiedT extends ExtendedT {
 		setQualifierT(qualifierT);
 	}
 
+	@Nullable
 	@Override
 	public T getQualifierT() {
 		final T enclosingT = getEnclosingT();
