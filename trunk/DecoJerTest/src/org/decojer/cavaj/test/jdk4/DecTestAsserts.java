1c8b0b240dbff4dc164a713b2b7224d0bed0b713
andrePankraz@gmail.com <andrePankraz@gmail.com@295ee101-0993-024e-5712-d33352bf8b4c>
2014-04-22 10:15:16 +0000

    assert fix

--- trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk4/DecTestAsserts.java
+++ trunk/DecoJerTest/src/org/decojer/cavaj/test/jdk4/DecTestAsserts.java
@@ -2,18 +2,31 @@ package org.decojer.cavaj.test.jdk4;
 
 public class DecTestAsserts {
 
+	int i = 1;
+
 	public void testAssert(final long l1, final long l2) {
+		System.out.println("PRE");
 		assert l1 != 0L;
+		System.out.println("POST");
 		assert l1 != l2;
 		assert false;
 		assert true;
 	}
 
+	public void testAssertCatch() {
+		try {
+			System.out.println("CATCH");
+			assert i < 0 : i;
+		} catch (RuntimeException e) {
+			assert i > 0 : e;
+		}
+		System.out.println("POST");
+	}
+
 	public void testMessageAssert(final long l1, final long l2) {
 		assert l1 < 0 : l1;
-		assert l1 > 0 ? l1 < l2 : l1 > l2 : (l1 >>> 1) + l2
-				+ "complex expression " + (l1 > l2 && l2 > -1) + l1 * l2 + l1
-				+ l2 + (l1 + l2);
+		assert l1 > 0 ? l1 < l2 : l1 > l2 : (l1 >>> 1) + l2 + "complex expression "
+				+ (l1 > l2 && l2 > -1) + l1 * l2 + l1 + l2 + (l1 + l2);
 	}
 
 }
