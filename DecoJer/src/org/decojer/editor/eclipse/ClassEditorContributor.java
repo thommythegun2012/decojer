2f45839dda101a0c875fb31d7755f7d08590aa8e
andrePankraz <andrePankraz@gmail.com>
2015-01-13 09:44:12 +0000

    small test to save decompiler file for searching etc

--- DecoJer/src/org/decojer/editor/eclipse/ClassEditorContributor.java
+++ DecoJer/src/org/decojer/editor/eclipse/ClassEditorContributor.java
@@ -16,12 +16,15 @@
 
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License,
  * a covered work must retain the producer line in every Java Source Code
  * that is created using DecoJer.
  */
 package org.decojer.editor.eclipse;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
@@ -30,9 +33,12 @@ import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.ide.IDE;
@@ -47,8 +53,10 @@ import org.eclipse.ui.texteditor.ITextEditorActionConstants;
  * contributors for the individual editors in the multi-page editor.
  */
 public class ClassEditorContributor extends MultiPageEditorActionBarContributor {
+
 	private IEditorPart activeEditorPart;
-	private Action sampleAction;
+
+	private Action decompileToFile;
 
 	/**
 	 * Creates a multi-page contributor.
@@ -60,33 +68,65 @@ public class ClassEditorContributor extends MultiPageEditorActionBarContributor 
 
 	@Override
 	public void contributeToMenu(final IMenuManager manager) {
-		final IMenuManager menu = new MenuManager("Editor &Menu");
+		final IMenuManager menu = new MenuManager("&DecoJer");
 		manager.prependToGroup(IWorkbenchActionConstants.MB_ADDITIONS, menu);
-		menu.add(this.sampleAction);
+		menu.add(this.decompileToFile);
 	}
 
 	@Override
 	public void contributeToToolBar(final IToolBarManager manager) {
 		manager.add(new Separator());
-		manager.add(this.sampleAction);
+		manager.add(this.decompileToFile);
 	}
 
 	private void createActions() {
-		this.sampleAction = new Action() {
+		this.decompileToFile = new Action() {
+
 			@Override
 			public void run() {
-				MessageDialog.openInformation(null, "DecoJer", "Sample Action Executed");
+				final IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow();
+
+				final FileDialog dialog = new FileDialog(activeWorkbenchWindow.getShell(), SWT.OPEN);
+				dialog.setFilterExtensions(new String[] { "*.jar" });
+				dialog.setFilterPath("d:\\");
+				final String file = dialog.open();
+				if (file == null) {
+					return;
+				}
+				final ClassEditor classEditor = (ClassEditor) activeWorkbenchWindow.getActivePage()
+						.getActiveEditor();
+				if (classEditor == null) {
+					return;
+				}
+				FileOutputStream fos = null;
+				try {
+					fos = new FileOutputStream(file);
+					classEditor.getDu().decompileAll(fos);
+					MessageDialog.openInformation(null, "DecoJer", "File saved.");
+				} catch (final IOException e) {
+					e.printStackTrace();
+				} finally {
+					if (fos != null) {
+						try {
+							fos.close();
+						} catch (final IOException e) {
+							// ignore
+						}
+					}
+				}
 			}
+
 		};
-		this.sampleAction.setText("Sample Action");
-		this.sampleAction.setToolTipText("Sample Action tool tip");
-		this.sampleAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+		this.decompileToFile.setText("Decompile To File...");
+		this.decompileToFile.setToolTipText("Decompile source to external file");
+		this.decompileToFile.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
 				.getImageDescriptor(IDE.SharedImages.IMG_OBJS_TASK_TSK));
 	}
 
 	/**
 	 * Returns the action registed with the given text editor.
-	 * 
+	 *
 	 * @param editor
 	 *            editor
 	 * @param actionID
@@ -134,4 +174,5 @@ public class ClassEditorContributor extends MultiPageEditorActionBarContributor 
 			actionBars.updateActionBars();
 		}
 	}
+
 }
