b775ba2f54c37907e61e3a93e46415c773886ff0
andrePankraz <andrePankraz@gmail.com>
2015-05-29 08:13:31 +0000

    UrlEncode utf8

--- DecoJerWeb/src/org/decojer/web/util/Uploads.java
+++ DecoJerWeb/src/org/decojer/web/util/Uploads.java
@@ -16,7 +16,7 @@
 
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License,
  * a covered work must retain the producer line in every Java Source Code
  * that is created using DecoJer.
@@ -24,6 +24,7 @@
 package org.decojer.web.util;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,10 +43,11 @@ import com.google.appengine.api.channel.ChannelServiceFactory;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
+import com.google.common.base.Charsets;
 
 /**
  * Uploads.
- * 
+ *
  * @author André Pankraz
  */
 public class Uploads {
@@ -90,69 +92,77 @@ public class Uploads {
 		final Map<Key, Entity> uploadKeys2Entities = DatastoreServiceFactory.getDatastoreService()
 				.get(uploadKeys);
 		boolean channel = false;
-		final StringBuilder sb = new StringBuilder("<ul>");
-		for (final Key uploadKey : uploadKeys) {
-			final Upload upload = new Upload(uploadKeys2Entities.get(uploadKey));
-			final String filename = upload.getFilename();
-
-			if (upload.getSourceBlobKey() != null) {
-				String sourcename;
-				if (upload.getTds().longValue() == 1L) {
-					final int pos = filename.lastIndexOf('.');
-					sourcename = (pos == -1 ? filename : filename.substring(0, pos)) + ".java";
-				} else {
-					final int pos = filename.lastIndexOf('.');
-					sourcename = (pos == -1 ? filename : filename.substring(0, pos))
-							+ "_source.zip";
-				}
-				sb.append("<li><a href='/download/").append(URLEncoder.encode(sourcename))
-						.append("?u=")
-						.append(URLEncoder.encode(upload.getSourceBlobKey().getKeyString()))
-						.append("' target='_blank'>").append(sourcename).append("</a>");
-				if (upload.getTds() > 1) {
-					sb.append(" (").append(upload.getTds()).append(" classes)");
-				} else if (upload.getSourceBlobKey() != null) {
-					sb.append(" (<a href='/?u=")
-							.append(URLEncoder.encode(upload.getSourceBlobKey().getKeyString()))
-							.append("'>View</a>)");
-				}
-			} else if (upload.getError() != null) {
-				sb.append("<li>").append(filename).append(" ERROR");
-			} else {
-				channel = true;
-				sb.append("<li>").append(filename).append(" ...decompiling...");
-				if (upload.getTds() > 1) {
-					sb.append(" (").append(upload.getTds()).append(" artefacts)");
-				}
-			}
-			sb.append("</li>");
-		}
-		sb.append("</ul>");
-
-		if (channel) {
-			sb.append("<script type='text/javascript' src='/_ah/channel/jsapi'></script>")
-					.append("<script>").append("  channel = new goog.appengine.Channel('")
-					.append(getChannelToken(httpSession)).append("');")
-					.append("  socket = channel.open();")
-					.append("  socket.onmessage = function(msg) { window.location.reload(); };")
-					.append("</script>");
-		}
-
-		final String u = req.getParameter("u");
-		if (u == null) {
-			return sb.toString();
-		}
 		try {
-			final String source = new String(IO.toBytes(new BlobstoreInputStream(new BlobKey(u))),
-					"UTF-8");
+			final StringBuilder sb = new StringBuilder("<ul>");
+			for (final Key uploadKey : uploadKeys) {
+				final Upload upload = new Upload(uploadKeys2Entities.get(uploadKey));
+				final String filename = upload.getFilename();
 
-			sb.append("<hr /><pre class=\"brush: java\">")
-					.append(source)
-					.append("</pre><script type=\"text/javascript\">SyntaxHighlighter.all()</script>");
-		} catch (final IOException e) {
-			LOGGER.log(Level.WARNING, "Couldn't write source!", e);
+				if (upload.getSourceBlobKey() != null) {
+					String sourcename;
+					if (upload.getTds().longValue() == 1L) {
+						final int pos = filename.lastIndexOf('.');
+						sourcename = (pos == -1 ? filename : filename.substring(0, pos)) + ".java";
+					} else {
+						final int pos = filename.lastIndexOf('.');
+						sourcename = (pos == -1 ? filename : filename.substring(0, pos))
+								+ "_source.zip";
+					}
+					sb.append("<li><a href='/download/")
+							.append(URLEncoder.encode(sourcename, Charsets.UTF_8.name()))
+							.append("?u=")
+							.append(URLEncoder.encode(upload.getSourceBlobKey().getKeyString(),
+									Charsets.UTF_8.name())).append("' target='_blank'>")
+							.append(sourcename).append("</a>");
+					if (upload.getTds() > 1) {
+						sb.append(" (").append(upload.getTds()).append(" classes)");
+					} else if (upload.getSourceBlobKey() != null) {
+						sb.append(" (<a href='/?u=")
+								.append(URLEncoder.encode(upload.getSourceBlobKey().getKeyString(),
+										Charsets.UTF_8.name())).append("'>View</a>)");
+					}
+				} else if (upload.getError() != null) {
+					sb.append("<li>").append(filename).append(" ERROR");
+				} else {
+					channel = true;
+					sb.append("<li>").append(filename).append(" ...decompiling...");
+					if (upload.getTds() > 1) {
+						sb.append(" (").append(upload.getTds()).append(" artefacts)");
+					}
+				}
+				sb.append("</li>");
+			}
+			sb.append("</ul>");
+
+			if (channel) {
+				sb.append("<script type='text/javascript' src='/_ah/channel/jsapi'></script>")
+						.append("<script>")
+						.append("  channel = new goog.appengine.Channel('")
+						.append(getChannelToken(httpSession))
+						.append("');")
+						.append("  socket = channel.open();")
+						.append("  socket.onmessage = function(msg) { window.location.reload(); };")
+						.append("</script>");
+			}
+
+			final String u = req.getParameter("u");
+			if (u == null) {
+				return sb.toString();
+			}
+			try {
+				final String source = new String(
+						IO.toBytes(new BlobstoreInputStream(new BlobKey(u))), "UTF-8");
+
+				sb.append("<hr /><pre class=\"brush: java\">")
+						.append(source)
+						.append("</pre><script type=\"text/javascript\">SyntaxHighlighter.all()</script>");
+			} catch (final IOException e) {
+				LOGGER.log(Level.WARNING, "Couldn't write source!", e);
+			}
+			return sb.toString();
+		} catch (final UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
 		}
-		return sb.toString();
 	}
 
 }
